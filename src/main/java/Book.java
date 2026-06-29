370356e6d3cf12a11609834fb5a4f2f6c18055bd
Justin Carmona <justin.carmona.123@gmail.com>
2026-06-29 12:19:13 -0400

    Created foundation of Book. Mutators, accessors, and constructors are all added.

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,5 +1,66 @@
 package main.java;
 
-public class Book {
-    
+public class Book implements Comparable<Book> {
+    private String author;
+    private String title;
+    private String ISBN;
+    private boolean checkedOut;
+
+    public Book() {
+        title = "N/A";
+        author = "N/A";
+        ISBN = "N/A";
+        checkedOut = false;
+    }
+
+    public Book(String author, String title, String ISBN, boolean checkedOut) {
+        this.author = author;
+        this.title = title;
+        this.ISBN = ISBN;
+        this.checkedOut = false;
+    }
+
+    public Book(Book other) {
+        this.author = other.author;
+        this.title = other.title;
+        this.ISBN = other.ISBN;
+        this.checkedOut = other.checkedOut;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public boolean isCheckedOut() {
+        return checkedOut;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setISBN(String isbn) {
+        this.ISBN = isbn;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        this.checkedOut = checkedOut;
+    }
+
+    @Override
+    public int compareTo(Book other) {
+        return this.title.compareTo(other.title);
+    }
 }
